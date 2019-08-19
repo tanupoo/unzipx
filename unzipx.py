9d99da0e0492d68d6197a7678144efb69c5a83d1
Shoichi Sakane <github-shoichi@tanu.org>
2019-08-19 11:11:55 +0900

    enabled to extract directories.

--- unzipx.py
+++ unzipx.py
@@ -2,6 +2,7 @@
 
 import zipfile
 import argparse
+from os import makedirs
 import sys
 
 ap = argparse.ArgumentParser(
@@ -20,8 +21,10 @@ ap.add_argument("-x", action="store_true", dest="extract_mode",
                 help="extract the contents.  default is to list the files.")
 ap.add_argument("-s", action="store_true", dest="redecode_cp932",
                 help="extract the filename as cp932.")
-ap.add_argument("-v", "--verbose", action="store_true", dest="verbose",
-                help="enable verbose mode.")
+ap.add_argument("-R", action="store_false", dest="recursive",
+                help="extract only the files, not including the directories.")
+ap.add_argument("-q", "--quiet", action="store_false", dest="verbose",
+                help="enable quiet mode.")
 opt = ap.parse_args()
 
 file_list = None
@@ -40,16 +43,35 @@ with zipfile.ZipFile(opt.zip_file) as z:
     if opt.password:
         z.setpassword(bytes(opt.password, "ascii"))
     for zi in z.infolist():
+        # get the filename including the path.
         if opt.redecode_cp932:
             filename = zi.filename.encode("cp437").decode("cp932")
         else:
             filename = zi.filename
+        # get the path and filename.
+        if filename.startswith("/"):
+            raise ValueError("ERROR: starting with a slash must not be allowed.")
+        elif filename.find("/") > 0:
+            path = filename[:filename.rindex("/")]
+        else:
+            path = None
+        # check whether encrypted.
         if (zi.flag_bits & 0x1) and opt.password is None:
-            print("ERROR: password required. {} is encrypted.".format(filename))
+            print("ERROR: password required. {} is encrypted.".format(path[-1]))
             exit(1)
+        # extract if needed.
         if opt.extract_mode:
             if file_list is None or n in file_list:
-                postfix = filename[filename.rindex("."):]
+                # create directories.
+                if path is not None and opt.recursive:
+                    try:
+                        makedirs(path, mode=511, exist_ok=False)
+                    except FileExistsError:
+                        pass
+                    else:
+                        if opt.verbose:
+                            print("{} has been created.".format(path))
+                # extract the file
                 with open(filename, "wb") as fd:
                     try:
                         fd.write(z.read(zi))
@@ -62,7 +84,16 @@ with zipfile.ZipFile(opt.zip_file) as z:
                             print(e)
                 if opt.verbose:
                     print("extract {} into {}".format(n, filename))
-            n += 1
         else:
-            print(filename)
-
+            if path is not None:
+                for i,p in enumerate(path.split("/")):
+                    if i == 0:
+                        print("{:d}: ".format(n), end="")
+                    elif i > 0:
+                        print("  "*i, end="")
+                    print(p)
+                print("  "*(1+i), filename)
+            else:
+                print("{:d}: {}".format(n,filename))
+        #
+        n += 1
